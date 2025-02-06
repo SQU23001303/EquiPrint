a7d8bd6c9639fcb08f4b401a4597d9dd2f1ab294
SQU23001303 <157488287+SQU23001303@users.noreply.github.com>
2025-02-06 14:44:55 +0000

    Initialisation of "EquiPrint" Group Project

---
+++ script.js
@@ -0,0 +1,9 @@
+const button = document.querySelector('.accessibility-button');
+
+button.addEventListener('click', () => {
+  // Toggle Dark Mode
+  document.body.classList.toggle('dark-mode');
+  
+  // Toggle Font Size (Example: Increase font size by 2px)
+  document.body.style.fontSize = document.body.style.fontSize === '18px' ? '16px' : '18px';
+});
