2b7147bb6c8490df0d345756fd20922ae9cd8823
Ben <benjamin7squire@outlook.com>
2025-02-18 20:05:46 +0000

    Looking at using php

--- prints.php
+++ prints.php
@@ -9,43 +9,7 @@
     <title>Prints</title>
 </head>
 <body>
-  <div class="nav-bar">
-    <!-- Logo Section -->
-    <img src="Images/EquiPrint_Logo_Light_svg.svg" alt="EquiPrint Logo">
-    <h1>EquiPrint</h1>
-
-    <!-- Navbar Section -->
-    <nav class="navbar navbar-expand-lg">
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <a class="nav-link" href="#">PRINTS</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">ABOUT&nbsp;US</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">CONTACT</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">FAQ</a>
-          </li>
-          <li class="nav-item d-lg-none mt-2">
-            <!-- Login button inside collapse for mobile view -->
-            <button class="login-btn w-100">Login</button>
-          </li>
-        </ul>
-      </div>
-    </nav>
-
-    <!-- Login Button Outside for Desktop -->
-    <div class="d-none d-lg-block">
-      <button class="login-btn">Login</button>
-    </div>
-  </div>
+  <?php include('navbar.php'); ?>
 
   <div class="container">
     <div class="sidebar">
