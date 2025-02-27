40fb47ebe3e20f5827196ac23ebc8962d0bbeb7f
SQU23001303 <157488287+SQU23001303@users.noreply.github.com>
2025-02-27 15:01:06 +0000

    CSS update

--- Includes/light-sidebar.inc.php
+++ Includes/light-sidebar.inc.php
@@ -2,16 +2,16 @@
 <div class="container">
     <div class="sidebar">
       <!-- Sidebar content like links, images, etc. -->
-      <button class="accessibility-button" aria-label="">
+      <button class="accessibility-sidebar" aria-label="">
         <img src="Images/W3schools.png.svg" alt="">
       </button>
-      <button class="accessibility-button" aria-label="">
+      <button class="accessibility-sidebar" aria-label="">
         <img src="Images/Vector.svg" alt="">
       </button>
-      <button class="accessibility-button" aria-label="">
+      <button class="accessibility-sidebar" aria-label="">
         <img src="Images/Vector-1.svg" alt="">
       </button>
-      <button class="accessibility-button" aria-label="">
+      <button class="accessibility-sidebar" aria-label="">
         <img src="Images/Vector-2.svg" alt="">
       </button>
     </div>
