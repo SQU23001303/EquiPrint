6931e00289dd07ec34ac93fcd8cdb212d43f6715
SQU23001303 <157488287+SQU23001303@users.noreply.github.com>
2025-03-06 12:14:45 +0000

    Footer and NavBar worked on

--- Includes/light-header.inc.php
+++ Includes/light-header.inc.php
@@ -27,13 +27,22 @@
             <a class="nav-link" href="faq.php">FAQ</a>
           </li>
           <li class="nav-item d-lg-none mt-2">
-            <!-- Login button inside collapse for mobile view -->
+            <button class="navbar-img" aria-label="">
+              <img src="Images/Cart Icon.svg" alt="Shopping Basket">
+            </button>
+          </li>
+          <li class="nav-item d-lg-none mt-2">
             <button class="login-btn w-100">Login</button>
           </li>
         </ul>
       </div>
     </nav>
 
+    <div class="d-none d-lg-block">
+      <button class="navbar-img" aria-label="">
+        <img src="Images/CartIcon.png" alt="Shopping Basket">
+      </button>
+    </div>
     <!-- Login Button Outside for Desktop -->
     <div class="d-none d-lg-block">
       <button class="login-btn">Login</button>
--- index.php
+++ index.php
@@ -50,9 +50,31 @@
         <h3>Accessibility Lead</h3>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
     </div>
-
-  
 </div>
+
+<footer>
+    <div class="footer-left">
+        <img src="Images/EquiPrint_Logo_Light_svg.svg" alt="EquiPrint Logo"> 
+        <div>
+            <strong>EquiPrint</strong><br>
+            3D Printing Made Accessible<br>
+            ©2025 EquiPrint. All Rights Reserved
+        </div>
+    </div>
+    <div class="footer-links">
+        <a href="index.php">Index</a>
+        <a href="prints.php">Prints</a>
+        <a href="about-us.php">About Us</a>
+        <a href="contact.php">Contact</a>
+        <a href="faq.php">FAQ</a>
+    </div>
+    <div class="footer-right">
+        <a href="#">X (Twitter)</a>
+        <a href="#">LinkedIn</a>
+        <a href="#">Facebook</a>
+    </div>
+</footer>
+
 
   
   <script src="script.js"></script>
