2c4da1463476f174145f1d2e26c234241334daaf
SQU23001303 <157488287+SQU23001303@users.noreply.github.com>
2025-03-06 09:52:47 +0000

    Changes to Index page Sections

--- index.php
+++ index.php
@@ -25,29 +25,30 @@
     </div>
   </div>
 
-  <!-- CHANGES HERE -->
-  <div class="accessibility-container">
-    <div class="accessibility-box">
-        <img src="path/to/your/icon1.png" alt="Accessibility Lead Icon" class="accessibility-icon">
+  <div class="container-sections">
+    <div class="section">
+      <hr>
+      <h3>Accessibility Lead</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    </div>
+    <div class="section">
+        <hr>
         <h3>Accessibility Lead</h3>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
     </div>
-    <div class="accessibility-box">
-        <img src="path/to/your/icon2.png" alt="File Alt Icon" class="accessibility-icon">
+    <div class="section">
+        <hr>
         <h3>Accessibility Lead</h3>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
     </div>
-    <div class="accessibility-box">
-        <img src="path/to/your/icon3.png" alt="Check Circle Icon" class="accessibility-icon">
+    <div class="section">
+        <hr>
+        <img src="Images/Vector-2.svg" alt="Accessibility Lead Icon" class="accessibility-icon">
         <h3>Accessibility Lead</h3>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
     </div>
-    <!-- New added box -->
-    <div class="accessibility-box">
-        <img src="path/to/your/icon4.png" alt="New Icon" class="accessibility-icon">
-        <h3>New Header</h3>
-        <p>New paragraph text goes here. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-    </div>
+
+  
 </div>
 
   
