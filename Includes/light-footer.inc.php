94d20d5322a9ae26b1f6678abd6af51dea3622d1
SQU23001303 <157488287+SQU23001303@users.noreply.github.com>
2025-03-06 15:21:28 +0000

    Footer complete

---
+++ Includes/light-footer.inc.php
@@ -0,0 +1,22 @@
+<footer>
+    <div class="footer-left">
+        <img src="Images/EquiPrint_Logo_Light_svg.svg" alt="EquiPrint Logo"> 
+        <div>
+            <strong>EquiPrint</strong><br>
+            3D Printing Made Accessible<br>
+            ©2025 EquiPrint. All Rights Reserved
+        </div>
+    </div>
+    <div class="footer-links">
+        <a href="index.php">Index</a>
+        <a href="prints.php">Prints</a>
+        <a href="about-us.php">About Us</a>
+        <a href="contact.php">Contact</a>
+        <a href="faq.php">FAQ</a>
+    </div>
+    <div class="footer-right">
+        <a href="#">Instagram</a>
+        <a href="#">LinkedIn</a>
+        <a href="#">Facebook</a>
+    </div>
+</footer>
--- index.php
+++ index.php
@@ -52,31 +52,7 @@
     </div>
 </div>
 
-<footer>
-    <div class="footer-left">
-        <img src="Images/EquiPrint_Logo_Light_svg.svg" alt="EquiPrint Logo"> 
-        <div>
-            <strong>EquiPrint</strong><br>
-            3D Printing Made Accessible<br>
-            ©2025 EquiPrint. All Rights Reserved
-        </div>
-    </div>
-    <div class="footer-right">
-        <a href="index.php">Index</a>
-        <a href="prints.php">Prints</a>
-        <a href="about-us.php">About Us</a>
-        <a href="contact.php">Contact</a>
-        <a href="faq.php">FAQ</a>
-    </div>
-    <div class="footer-right">
-        <a href="#">Instagram</a>
-        <a href="#">LinkedIn</a>
-        <a href="#">Facebook</a>
-    </div>
-</footer>
-
-
-  
+  <?php include('includes/light-footer.inc.php'); ?>
   <script src="script.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
