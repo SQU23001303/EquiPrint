ea24b65971fd7427025e512f35da6d7d56a1792f
Will Young <willyoungslk@gmail.com>
2025-03-20 19:14:34 +0000

    Added Register button to the left of Login button for desktop and mobile users. Also added CSS styling to match the Login button.

--- Includes/light-header.inc.php
+++ Includes/light-header.inc.php
@@ -31,22 +31,28 @@
               <img src="Images/Cart Icon.svg" alt="Shopping Basket">
             </button>
           </li>
-          <li class="nav-item d-lg-none mt-2">
-            <button class="login-btn w-100">Login</button>
-          </li>
-        </ul>
-      </div>
-    </nav>
+
+    <!-- Added register button next to login button for Desktop. -->
+    <div class="d-none d-lg-block">
+      <button class="auth-btn register-btn">Register</button>
+      <button class="auth-btn login-btn">Login</button>
+    </div>
+</div>
+<br>
+
+    <!-- Added register button next to login button for Mobile. -->
+    <li class="nav-item d-lg-none mt-2">
+      <button class="auth-btn register-btn w-100">Register</button>
+    </li>
+    <li class="nav-item d-lg-none mt-2">
+      <button class="auth-btn login-btn w-100">Login</button>
+    </li>
+  </ul>
+</div>
+</nav>
 
     <div class="d-none d-lg-block">
       <button class="navbar-img" aria-label="">
         <img src="Images/CartIcon.png" alt="Shopping Basket">
       </button>
     </div>
-    <!-- Login Button Outside for Desktop -->
-    <div class="d-none d-lg-block">
-      <button class="login-btn">Login</button>
-    </div>
-  </div>
-  <br>
-</div>
