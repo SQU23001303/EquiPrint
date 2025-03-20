7dff8af74ff94af126974ed41fb23185c538c063
Will Young <willyoungslk@gmail.com>
2025-03-20 20:39:06 +0000

    Updated registration form name attributes to enable the functionality of the registration script. Still non functioning, so checking for other code errors.

--- register.php
+++ register.php
@@ -51,28 +51,22 @@
   <!-- Registration Form -->
   <main class="form-signin w-100 m-auto">
     <!-- POST method required for the registration php script. -->
-    <form action="registrationscript.php" method="POST">
-      <img class="mb-4" src="Images/EquiPrint_Logo_Light_svg.svg" alt="Equiprint Logo" width="72" height="57">
-      <h1 class="h3 mb-3 fw-normal">Create an account <br> with us!</h1>
+    <form action="register.php" method="POST">
+    <img class="mb-4" src="Images/EquiPrint_Logo_Light_svg.svg" alt="Equiprint Logo" width="72" height="57">
+    <h1 class="h3 mb-3 fw-normal">Create an account <br> with us!</h1>
 
-      <div class="form-floating">
-        <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+    <div class="form-floating">
+        <input type="email" class="form-control" id="floatingInput" name="email" required placeholder="name@example.com">
         <label for="floatingInput">Email address</label>
-      </div>
-      <div class="form-floating">
-        <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+    </div>
+    <div class="form-floating">
+        <input type="password" class="form-control" id="floatingPassword" name="password" required placeholder="Password">
         <label for="floatingPassword">Password</label>
-      </div>
+    </div>
 
-      <div class="form-check text-start my-3">
-        <input class="form-check-input" type="checkbox" value="remember-me" id="flexCheckDefault">
-        <label class="form-check-label" for="flexCheckDefault">
-          Remember me
-        </label>
-      </div>
-      <!-- Added 'register-btn' class for styling -->
-      <button class="btn register-btn w-100 py-2" type="submit">Register account</button>
-    </form>
+    <button class="btn register-btn w-100 py-2" type="submit" name="register">Register account</button>
+</form>
+
   </main>
 
   <?php include('includes/light-footer.inc.php'); ?>
