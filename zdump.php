42533527377306663de372824598ffdb8caaeb5e
Will Young <willyoungslk@gmail.com>
2025-03-20 19:08:11 +0000

    Copied the navigation bar code to the zdump file in preparation for changes to be made. (The addition of a register button.)

--- zdump.php
+++ zdump.php
@@ -46,3 +46,62 @@
         <h3>New Header</h3>
         <p>New paragraph text goes here. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
     </div>
+
+
+
+
+
+
+
+    <!-- navbar.php -->
+<br>
+<div class="nav-bar">
+    <!-- Logo Section -->
+    <a href="index.php" class="logo">
+      <img src="Images/EquiPrint_Logo_Light_svg.svg" alt="EquiPrint Logo">
+    </a>
+    <h1><a href="index.php">EquiPrint</a></h1>
+
+    <!-- Navbar Section -->
+    <nav class="navbar navbar-expand-lg">
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link" href="prints.php">PRINTS</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="about-us.php">ABOUT&nbsp;US</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="contact.php">CONTACT</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="faq.php">FAQ</a>
+          </li>
+          <li class="nav-item d-lg-none mt-2">
+            <button class="navbar-img" aria-label="">
+              <img src="Images/Cart Icon.svg" alt="Shopping Basket">
+            </button>
+          </li>
+          <li class="nav-item d-lg-none mt-2">
+            <button class="login-btn w-100">Login</button>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+    <div class="d-none d-lg-block">
+      <button class="navbar-img" aria-label="">
+        <img src="Images/CartIcon.png" alt="Shopping Basket">
+      </button>
+    </div>
+    <!-- Login Button Outside for Desktop -->
+    <div class="d-none d-lg-block">
+      <button class="login-btn">Login</button>
+    </div>
+  </div>
+  <br>
+</div>
