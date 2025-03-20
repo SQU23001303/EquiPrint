89f55f5d73593e70760c02e20f5a4566b7a875af
Will Young <willyoungslk@gmail.com>
2025-03-20 23:05:31 +0000

    Log out script now working successfully. An error is currently encountered where the user begins logged in - This will need to be fixed. Additionally, CSS styling is needed for the logout button to match the website design.

--- logout.php
+++ logout.php
@@ -1,4 +1,5 @@
 <?php
+//LOGOUT SCRIPT WORKING SUCCESSFULLY. USER STARTS LOGGED IN, WHICH NEEDS TO BE RECTIFIED.
 session_start();
 session_destroy(); // Destroy the session
 header("Location: index.php"); // Redirect to homepage
